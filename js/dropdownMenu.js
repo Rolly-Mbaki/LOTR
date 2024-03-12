aa75c6e7ac737a411c5f788c88e5453dfc5e2549
Rolly Mbaki <rollymbakii@gmail.com>
2024-03-12 16:38:11 +0000

    mobile nav is bijna af

--- js/dropdownMenu.js
+++ js/dropdownMenu.js
@@ -1,8 +1,8 @@
 function dropdownMenu() {
     var x = document.getElementById("topnav");
-    if (x.style.display === "flex") {
+    if (x.style.display === "block") {
       x.style.display = "none";
     } else {
-      x.style.display = "flex";
+      x.style.display = "block";
     }
   }
