96269cb9e86b3cd175655ca2fdb5a7fc60084f18
Rolly Mbaki <rollymbakii@gmail.com>
2024-04-29 22:30:51 +0000

    Login toegevoegd, wanneer de gebruiker inlogd ziet hij zijn naam ipv gebruiker

--- index.ts
+++ index.ts
@@ -20,6 +20,11 @@ interface User {
   
 let message:string = "";
 let error:boolean = true;
+let user:User = {
+    username: "",
+    email:"",
+    password:""
+};
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}))
@@ -32,7 +37,8 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/home",(req,res)=>{
-    res.render("home");
+    // console.log(user)
+    res.render("home",{user:user});
 })
 
 app.get("/login",(req,res)=>{
@@ -43,15 +49,24 @@ app.post("/login", async (req,res)=>{
     try {
         await client.connect();
 
-        let userUsername = await client.db('LOTR').collection('Users').findOne({username:req.body.username.toString().toLowerCase()})
+        let username:User = await client.db('LOTR').collection('Users').findOne({username:req.body.username.toString().toLowerCase()})
 
-        if (userUsername == null) {
+        if (username == null) {
             message = "Gebruiker niet gevonden"
             error = true
             res.render("login",{message:message,error:error})
         }
-        else{
-            res.render("login",{message:message,error:error})
+        if (username!=null) {
+            const isPasswordMatch = await bcrypt.compare(req.body.password, username.password)
+            if (isPasswordMatch){
+                user = username;
+                res.redirect("home")
+            }
+            else{
+                message= "Fout wachtwoord"
+                error= true
+                res.render("login",{message:message,error:error})
+            }
         }
 
     } catch (e) {
@@ -101,7 +116,7 @@ app.post("/register", async (req,res)=>{
                 await client.db('LOTR').collection('Users').insertOne(newUser);
                 message = "Account succevol aangemaakt!"
                 error = false
-                res.render("home")
+                res.redirect("home")
             } 
             else if (req.body.password.length <= 7) {
                 message = "Wachtwoord bevat minder dan 8 karakaters"
@@ -127,20 +142,82 @@ app.post("/register", async (req,res)=>{
     } 
 });
 
+// app.post("/register", async (req,res)=>{
+
+//     let newUser:User = {
+//         username:req.body.username,
+//         email:req.body.email.toString().toLowerCase(),
+//         password:req.body.password
+//       }
+//       const saltRounds = 10
+//       const hashehPassword = await bcrypt.hash(newUser.password, saltRounds)
+
+//       newUser.password = hashehPassword
+      
+//       console.log(newUser)
+      
+//        try {
+//         await client.connect();
+        
+        
+//         let userEmail:User = await client.db('LOTR').collection('Users').findOne({email:req.body.email.toString().toLowerCase()})
+
+//         let userUsername: User = await client.db('LOTR').collection('Users').findOne({username:req.body.username})
+        
+//         console.log(userUsername)
+
+//         if (userEmail == null) {
+
+
+//             if (userUsername.username.toLowerCase() == newUser.username.toLowerCase()) {
+            
+//                 message = "Gebruikersnaam bestaat al"
+//                 error = true
+//                 res.render("register",{message:message,error:error})
+//             }  
+//             else if (req.body.password.length > 6) {
+//                 await client.db('LOTR').collection('Users').insertOne(newUser);
+//                 message = "Account succevol aangemaakt!"
+//                 error = false
+//                 res.redirect("home")
+//             } 
+//             else if (req.body.password.length <= 7) {
+//                 message = "Wachtwoord bevat minder dan 8 karakaters"
+//                 error = true
+//                 res.render("register",{message:message,error:error})
+//           }
+//         } 
+
+//         if (userEmail != null) {
+          
+//             message = "Email bestaat al"
+//             error = true
+//             res.render("register",{message:message,error:error})
+//         }
+        
+//     } catch (e) {
+//         console.error(e);
+//         error = true
+//         message = "Account maken mislukt, probeer terug opnieuw"
+//     } finally {
+//         await client.close();
+//     } 
+// });
+
 app.get("/blacklist",(req,res)=>{
-    res.render("blacklist");
+    res.render("blacklist",{user:user});
 })
 
 app.get("/fav",(req,res)=>{
-    res.render("fav");
+    res.render("fav",{user:user});
 })
 
 app.get("/suddenDeath",(req,res)=>{
-    res.render("suddenDeath");
+    res.render("suddenDeath",{user:user});
 })
 
 app.get("/tenRound",(req,res)=>{
-    res.render("tenRound");
+    res.render("tenRound",{user:user});
 })
 
 
